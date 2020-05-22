f413c2615a0d2ad9e6f23d8ac2f97a2af5bc6473
Eric Hernandez <ehernandez@magicleap.com>
2020-05-22 14:29:30 -0400

    Trying to make it less hardcoded using Vue.

---
+++ voicedemo copy.js
@@ -0,0 +1,41 @@
+var menudata = `{
+  "menuList": [
+    {
+      "id":"voice-menu",
+      "vocalCommand": "voice menu",
+      "isRootMenu": true,
+      // "menuItems": [
+      // {"id":"poster-menu","text": "Poster","type":"menu"},
+      // {"id":"food-menu","text": "Food","type":"menu"},
+      // {"id":"toy-menu","text": "Toy","type":"menu"}
+      // ]
+    },
+    // {
+    //   "id":"poster-menu",
+    //   "vocalCommand": "poster",
+    //   "menuItems": [
+    //   {"id":"back-to-the-future","text": "Back to the Future","type":"poster"},
+    //   {"id":"lego-movie","text": "Lego Movie","type":"poster"},
+    //   {"id":"matrix","text": "Matrix","type":"poster"}
+    //   ]
+    // },
+    // {
+    //   "id":"food-menu",
+    //   "vocalCommand": "food",
+    //   "menuItems": [
+    //   {"id":"pizza","text": "Pizza","type":"food"},
+    //   {"id":"wings","text": "Wings","type":"food"},
+    //   {"id":"ice-cream","text": "Ice Crea","type":"food"}
+    //   ]
+    // },
+    // {
+    //   "id":"toy-menu",
+    //   "vocalCommand": "toy",
+    //   "menuItems": [
+    //   {"id":"teddy-bear","text": "Teddy Bear","type":"toy"},
+    //   {"id":"train","text": "Train","type":"toy"},
+    //   {"id":"frog","text": "Frog","type":"toy"}
+    //   ]
+    // }
+  ]
+}`;
